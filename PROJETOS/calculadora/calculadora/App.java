597b56a6483283ef4cd0067b6afac91db5678c2d
MADARINHA <n-arlan12@hotmail.com>
2022-09-21 15:01:21 -0300

    testes

--- PROJETOS/calculadora/calculadora/App.java
+++ PROJETOS/calculadora/calculadora/App.java
@@ -2,6 +2,12 @@ package calculadora;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Holla, mundo!");
+        Circulo c1=new Circulo(4);
+        Quadrado q1=new Quadrado(2);
+        Processos process=new Processos();
+        System.out.format("\n\nÁrea do circulo: %f \nPerimetro do circulo: %f", c1.calcularArea(), c1.calcularPerimetro());
+        System.out.format("\n\nÁrea do quadrado: %f \nPerimetro do circulo: %f", q1.calcularArea(), q1.calcularPerimetro());
+        System.out.format("\n\nSoma das áreas anterioes: %f", process.somarAreas(c1, q1));
+        System.out.format("\n\nSoma dos Perimetros anterioes: %f", process.somarPerimetros(c1, q1));
     }
 }
---
+++ PROJETOS/calculadora/calculadora/Circulo.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+public class Circulo implements FormaGeometrica {
+    private double raio;
+
+    public double calcularArea(){
+        return(Math.PI * Math.pow(raio, 2));
+    }
+    public double calcularPerimetro(){
+        return(2*Math.PI*raio);
+    }
+    public Circulo(double raio){
+        this.raio=raio;
+    }
+}
+    
