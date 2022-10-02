40a0d5ec2d8db692b346f4d5edcbc8cd05ef867f
MADARINHA <n-arlan12@hotmail.com>
2022-10-02 18:10:08 -0300

    att

--- PROJETOS/atividades/lista2Questao20/Cliente.java
+++ PROJETOS/atividades/lista2Questao20/Cliente.java
@@ -1,11 +1,17 @@
 package lista2Questao20;
 
 public class Cliente extends Paciente {
+    private String data;
+    private String hora;
 
+    public String getData(){return this.data;}
+    public String getHora(){return this.hora;}
     public void exibeCleinte(Cliente c){
         //imprime cpf, nome e telefone do cliente
     }
     public Cliente(String cpf, String nome, String telefone, String data, String hora){
-        super(cpf, nome, telefone, data, hora);
+        super(cpf, nome, telefone);
+        this.data = data;
+        this.hora = hora;
     }
 }
--- PROJETOS/atividades/lista2Questao20/Paciente.java
+++ PROJETOS/atividades/lista2Questao20/Paciente.java
@@ -4,19 +4,13 @@ public abstract class Paciente {
     private String cpf;
     private String nome;
     private String telefone;
-    private String data;
-    private String hora;
     
     public String getCpf(){return this.cpf;}
     public String getNome(){return this.nome;}
     public String getTelefone(){return this.telefone;}
-    public String getData(){return this.data;}
-    public String getHora(){return this.hora;}
-    public Paciente(String cpf, String nome, String telefone, String data, String hora){
+    public Paciente(String cpf, String nome, String telefone){
         this.cpf = cpf;
         this.nome = nome;
         this.telefone = telefone;
-        this.data = data;
-        this.hora =  hora;
     }
 }
